4c4785311928217f57772f37d349f48cda9598ef
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-28 13:00:44 -0500

    added ++ instead of +1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+  //
+constructor(props){
+  // need super props to access this.state
+  super(props);
+
+  this.state = {
+    // add color or an array of colors here
+    color: 'blue',
+    // set click counter to 0
+    clickCounter: 0,
+    // add array of colors to change colors to more than or randomly
+    //use math.random nad this.state to reference the array and .length
+    // color: ['blue', 'pink', 'red', 'purple'],
+    // document.classID(refere the div)[ref the index/ it was a 0]... this.state.color[reference the array]
+    
+
+  }
 }
 
-export default App;
+onChange = (e) =>{
+  //set state to something else
+  this.setState({color: 'orange',
+                // make a click counter and call the state
+                // may need to change "++" to "+ 1"
+                // ++ mutates states directly
+                // .bind to recognize ".this"
+                clickCounter: this.state.clickCounter + 1 })
+                // if
+                // ADD if statement (see if that works)
+                console.log("click counter ===>", this.state.clickCounter)
+}
+
+onChangeToNextColor = (e) =>{
+  //set state to something else
+  this.setState({color: 'pink',
+                clickCounter: this.state.clickCounter++ })
+                console.log("click counter ===>", this.state.clickCounter)
+}
+
+  render() {
+    return (
+      //app is parent container
+      <div className="App">
+        <h1>Box Color Change. </h1>
+        <div className="colorBox" style={{backgroundColor: this.state.color}}
+          onClick={this.onChange}>
+          <p>Click Here</p>
+        </div>
+      </div>
+
+    )
+  }
+}
