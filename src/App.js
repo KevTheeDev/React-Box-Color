8e0e16a8a6ac3044e135f443517225a1ebcd3b6f
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-28 22:29:31 -0500

    try new statements

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const brownColor = 'brown';
+const blueColor = 'blue';
+
+
 export default class App extends Component{
 
-  //
 constructor(props){
   // need super props to access this.state
   super(props);
 
   this.state = {
     // add color or an array of colors here
-    color: 'blue',
+    color: blueColor,
     // set click counter to 0
-    clickCounter: 0,
+    clickCounter: 0}
     // add array of colors to change colors to more than or randomly
     //use math.random nad this.state to reference the array and .length
     // color: ['blue', 'pink', 'red', 'purple'],
     // document.classID(refere the div)[ref the index/ it was a 0]... this.state.color[reference the array]
-    
+    this.onChange = this.onChange.bind(this);
   }
-}
 
 onChange = (e) =>{
   //set state to something else
   // setState is a component lifetsyle method
-  this.setState({color: ['orange', 'blue'],
+  this.setState({color: 'orange',
                 // make a click counter and call the state
                 // may need to change "++" to "+ 1"
-                // ++ mutates states directly
+                // ++ mutates states directly, so it won't work
                 // .bind to recognize ".this"
                 clickCounter: this.state.clickCounter + 1 })
-                // if
                 // ADD if statement (see if that works)
-                if(this.state.color[0]){
-                  this.setState({ color: 'blue'})
-                }
+                const onChangeToNextColor = this.state.color === blueColor ? brownColor : blueColor;
+                this.setState({ color: onChangeToNextColor})
                 console.log("click counter ===>", this.state.clickCounter)
 }
 
